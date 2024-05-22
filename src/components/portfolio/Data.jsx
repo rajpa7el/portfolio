81b440305bbef8d4f435c1187b0a30488e8095e1
rajpa7el <rajpatel112003@gmail.com>
2024-05-22 06:07:55 -0230

    Updated Portfolio Section

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -1,54 +1,43 @@
-import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
-import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.jpg";
+import CantStop from "../../assets/Cant_stop.png";
+import Chat from "../../assets/Chat.png";
+import ChainReaction from "../../assets/ChainReaction.png";
+import Connect4 from "../../assets/Connect4.png";
+import Hand from "../../assets/Hand.png";
 
 export const projectsData = [
   {
     id: 1,
-    image: Work1,
-    title: "Web design",
-    category: "web",
+    image: CantStop,
+    title: "Can't Stop Game",
+    category: "Software Development",
+    link: "https://github.com/rajpa7el/Cant-Stop-Board-Game",
   },
   {
     id: 2,
-    image: Work2,
-    title: "App movil",
-    category: "app",
+    image: Chat,
+    title: "Real Time Chat",
+    category: "MERN stack",
+    link: "https://github.com/rajpa7el/realtime-chat",
   },
   {
     id: 3,
-    image: Work3,
-    title: "Brand design",
-    category: "design",
+    image: ChainReaction,
+    title: "Chain Reaction Game",
+    category: "Mobile Development",
+    link: "https://github.com/rajpa7el/chain_reaction_game",
   },
   {
     id: 4,
-    image: Work4,
-    title: "App movil",
-    category: "app",
+    image: Connect4,
+    title: "Connect-4",
+    category: "AI and game algorithms",
+    link: "https://www.youtube.com/watch?v=w75cjDDUgGc",
   },
   {
     id: 5,
-    image: Work5,
-    title: "Web design",
-    category: "web",
-  },
-];
-
-// projects
-export const projectsNav = [
-  {
-    name: "all",
-  },
-  {
-    name: "web",
-  },
-  {
-    name: "app",
-  },
-  {
-    name: "design",
+    image: Hand,
+    title: "Gesture Recognition",
+    category: "ML and Computer Vision",
+    link: "https://github.com/rajpa7el/Hand-Gesture-Recognition-CS3301",
   },
 ];
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,4 +1,3 @@
-// Portfolio.jsx
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -10,65 +9,73 @@ import "./portfolio.css";
 import { motion } from "framer-motion";
 
 const Portfolio = () => {
-    return (
-      <section className="portfolio section" id="portfolio">
-        <div className="container1">
-          <motion.h2
-            className="section__title"
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5 }}
-          >
-            Portfolio
-          </motion.h2>
-          <motion.span
-            className="section__subtitle"
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.2 }}
-          >
-            Most recent work
-          </motion.span>
-          <Swiper
-            spaceBetween={30}
-            centeredSlides={true}
-            loop={true}
-            autoplay={{
-              delay: 4000,
-              disableOnInteraction: false,
-            }}
-            pagination={{
-              clickable: true,
-              type: "progressbar",
-            }}
-            navigation={true}
-            modules={[Autoplay, Pagination, Navigation]}
-            className="mySwiper"
-          >
-            {projectsData.map((project) => (
-              <SwiperSlide key={project.id}>
-                <motion.div
-                  className="portfolio__item"
-                  initial={{ opacity: 0, scale: 0.9 }}
-                  animate={{ opacity: 1, scale: 1 }}
-                  transition={{ duration: 0.5, delay: 0.3 }}
-                >
+  return (
+    <section className="portfolio section" id="portfolio">
+      <div className="container1">
+        <motion.h2
+          className="section__title"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          Portfolio
+        </motion.h2>
+        <motion.span
+          className="section__subtitle"
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
+          Most recent work
+        </motion.span>
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          loop={true}
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+            type: "progressbar",
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {projectsData.map((project) => (
+            <SwiperSlide key={project.id}>
+              <motion.div
+                className="portfolio__item"
+                initial={{ opacity: 0, scale: 0.9 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5, delay: 0.3 }}
+              >
+                <a href={project.link} target="_blank" rel="noopener noreferrer">
                   <img
                     src={project.image}
                     alt={project.title}
                     className="portfolio__img"
                   />
-                  <h3 className="portfolio__title">{project.title}</h3>
-                  <span className="portfolio__category">{project.category}</span>
-                </motion.div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-      </section>
-    );
-  };
-  
-  export default Portfolio;
-  
+                </a>
+                <h3 className="portfolio__title">
+                  <a href={project.link} target="_blank" rel="noopener noreferrer">
+                    {project.title}
+                  </a>
+                </h3>
+                <span className="portfolio__category">
+                  <a href={project.link} target="_blank" rel="noopener noreferrer">
+                    {project.category}
+                  </a>
+                </span>
+              </motion.div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
 
+export default Portfolio;
